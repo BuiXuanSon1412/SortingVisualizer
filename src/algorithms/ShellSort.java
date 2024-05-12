c2be64e6d977157eaa33552de22e9bd409bf6b8d
vuducmanhpractisingcode <130825695+vuducmanhpractisingcode@users.noreply.github.com>
2024-05-12 23:27:07 +0700

    Update ShellSort.java

--- src/algorithms/ShellSort.java
+++ src/algorithms/ShellSort.java
@@ -3,8 +3,40 @@ package algorithms;
 import views.Visualizer;
 
 public class ShellSort extends SortAbstraction {
+
     public void sort(Visualizer visualizer) {
-        // implementation
-        
+        int num = visualizer.getNumberOfBars();
+
+        // define the initial gap between elements
+        int gap = 1;
+        while (gap < num / 3) {
+            gap = gap * 3 + 1; // Use Knuth formula: 3x + 1
+        }
+
+        // Start sort algorithm
+        while (gap > 0) {
+            for (int i = gap; i < num; i++) {
+                int temp = visualizer.getValue(i);
+                int j = i;
+
+                // move elements whose distance between them = gap
+                // and if current element > (j-gap)-th element
+                while (j >= gap && visualizer.getValue(j - gap) >= temp) {
+                    visualizer.setMarkedColor(j);
+                    visualizer.setIteratingColor(j - gap);
+                    visualizer.swap(j, j - gap);
+                    if (gap == 1) {
+                        visualizer.setSortedColor(j);
+                        visualizer.setSortedColor(j - gap);
+                    }
+                    j -= gap;
+                }
+                if (gap == 1)
+                    visualizer.setSortedColor(j);
+            }
+            // decrease the gap and start new iteration
+            gap = gap - 1;
+            // Set sorted color for the last swapped elements
+        }
     }
 }
